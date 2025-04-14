3582f22f96c9e647708a766ac2bddbdc0ab57244
abelgtzrs <32408235+abelgtzrs@users.noreply.github.com>
2025-04-14 15:50:57 -0400

    Created Thoughts mdoel

---
+++ models/Thought.js
@@ -0,0 +1,71 @@
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat"); // optional utility for formatting
+
+// Reaction Subdocument Schema
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// Thought Schema
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// Virtual: reactionCount
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+// Create Thought model
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
